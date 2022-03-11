31e1f2be3cec6c91757252c33538592451f1b38a
agsleonardo <agsleonardo@hotmail.com>
2022-03-11 01:00:36 -0300

    Realizada requisição para questões com token vindo do login

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -4,20 +4,27 @@ import { connect } from 'react-redux';
 import { fetchQuestions } from '../redux/actions';
 
 class Play extends Component {
+  state = {
+    loading: true,
+  }
+
   componentDidMount = () => {
     const { getQuestions, token } = this.props;
-    getQuestions('18ae77a022d944bb0864e3d41cb23ff4c2cf3793843a91757f08cc0e83cb21f1');
+    getQuestions(token);
   }
 
   render() {
+    const { questions, token } = this.props;
     return (
-      <Card />
+      <>
+        <Card />
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  trivia: state.trivia,
+  questions: state,
   token: state.token,
 })
 
