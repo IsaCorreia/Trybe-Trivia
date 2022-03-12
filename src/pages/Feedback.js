677fd7a936d7e47ad6d38ca8dd93f9dedc70ff15
Su Costa <stcosta55@gmail.com>
2022-03-12 00:44:39 -0300

    inclusa msg feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,38 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { connect } from 'react-redux';
 
 class Feedback extends Component {
+
+  getMessagesFeedback = (() => {
+    const{ questionAssertions } = this.props;
+    if (questionAssertions < 3) {
+      return 'Could be better...';
+    } else {
+      return 'Well Done!';
+    }
+  })
+
   render() {
+    const msgFeedback = this.getMessagesFeedback;
     return (
-      <div id="feedbackScreen">
+      <div id="feedback-Screen">
         <Header />
+        <div>
+          <h1 data-testid="feedback-text">{ msgFeedback }</h1>
+        </div>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  questionAssertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  questionAssertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
