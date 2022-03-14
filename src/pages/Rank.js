427cf5e2966529ed0354fb93665ddc8d3673944b
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-03-14 17:25:12 -0300

    Tela de Ranking - puxando codigo que ja estava pronto

--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import PlayerRank from '../components/PlayerRank';
+
+const mockStorageRank = [
+  {
+    name: 'fulano',
+    score: 1,
+    gravatarEmail: 'fulano@email.com',
+  },
+  {
+    name: 'cliclano',
+    score: 2,
+    gravatarEmail: 'cliclano@email.com',
+  },
+];
 
 class Rank extends Component {
+  state = {
+    rank: mockStorageRank,
+  };
+
   render() {
+    const { rank } = this.state;
     return (
-      <p>Rank</p>
+      <>
+        {rank.map((item, index) => (
+          <PlayerRank info={ item } index={ index } key={ index } />
+        ))}
+      </>
     );
   }
 }
