3acf58fe61b5ac441fc44d85c01e96846511dc8a
agsleonardo <agsleonardo@hotmail.com>
2022-03-14 23:56:36 -0300

    Criando css para Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Form from '../components/Form';
+import logoTrivia from '../trivia.png';
+import './Login.css';
 
 class Login extends Component {
   render() {
     return (
-      <>
-        <Link to="/Config">
-          <button type="button" data-testid="btn-settings">Configs</button>
-        </Link>
-        <Form />
-      </>
+      <main>
+        <section>
+          <img src={ logoTrivia } alt="Logo Trivia" />
+        </section>
+        <section>
+          <Link to="/Config">
+            <button type="button" data-testid="btn-settings">Configs</button>
+          </Link>
+          <Form />
+        </section>
+      </main>
     );
   }
 }
