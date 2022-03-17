d73073f0021a46d5f57c8d6ccd9dfd75c27e347e
agsleonardo <agsleonardo@hotmail.com>
2022-03-16 22:44:02 -0300

    atualizando branch

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -74,20 +74,19 @@ class Answers extends Component {
   render() {
     const {
       correct = '',
-      // wrong = [],
+      wrong = [],
       disable,
       classCorrect,
       classWrong,
-      shuffledAnswers,
     } = this.props;
-    // const correctAnswer = { answer: correct, tag: 'correct-answer' };
-    // const incorrectAnswers = wrong && wrong.reduce((acc, cur, idx) => {
-    //   acc.push({ answer: cur, tag: `wrong-answer-${idx}` });
-    //   return acc;
-    // }, []);
-    // const allAnswers = [correctAnswer, ...incorrectAnswers];
-    // const RANDOM_POS = 0.5;
-    // const shuffledAnswers = allAnswers.sort(() => RANDOM_POS - Math.random()); // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way,)%20%3D%3E%200.5%20%2D%20Math.
+    const correctAnswer = { answer: correct, tag: 'correct-answer' };
+    const incorrectAnswers = wrong && wrong.reduce((acc, cur, idx) => {
+      acc.push({ answer: cur, tag: `wrong-answer-${idx}` });
+      return acc;
+    }, []);
+    const allAnswers = [correctAnswer, ...incorrectAnswers];
+    const RANDOM_POS = 0.5;
+    const shuffledAnswers = allAnswers.sort(() => RANDOM_POS - Math.random()); // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way,)%20%3D%3E%200.5%20%2D%20Math.
 
     return (
       shuffledAnswers
@@ -116,7 +115,7 @@ class Answers extends Component {
 
 Answers.propTypes = {
   correct: PropTypes.string.isRequired,
-  // wrong: PropTypes.string.isRequired,
+  wrong: PropTypes.string.isRequired,
   disable: PropTypes.bool.isRequired,
   saveAnswerSelected: PropTypes.func.isRequired,
   isNextVisible: PropTypes.func.isRequired,
@@ -124,7 +123,6 @@ Answers.propTypes = {
   classCorrect: PropTypes.string.isRequired,
   classWrong: PropTypes.string.isRequired,
   answerSelected: PropTypes.objectOf(PropTypes.string).isRequired,
-  shuffledAnswers: PropTypes.objectOf(PropTypes.any).isRequired,
   assertionValue: PropTypes.number.isRequired,
   questionList: PropTypes.arrayOf(PropTypes.object).isRequired,
   ADDAssertions: PropTypes.func.isRequired,
